f50a9dd30fade79967a17dd757f7dca6a8d52d48
Barry Rollan <barry.rollan@gmail.com>
2020-03-15 11:55:45 -0400

    added trainer.js to schemas/documents

---
+++ studio/schemas/documents/trainer.js
@@ -0,0 +1,57 @@
+import {format} from 'date-fns'
+
+export default {
+  name: 'trainer',
+  type: 'document',
+  title: 'Trainer',
+  fields: [
+    {
+      name: 'name',
+      type: 'string',
+      title: 'Name',
+      description: 'First and Last Name',
+      validation: Rule => Rule.required()
+    },
+    {
+        name: 'image',
+        type: 'image',
+        title: 'Picture',
+        validation: Rule => Rule.required()
+      },
+    {
+      name: 'bioA',
+      type: 'string',
+      title: 'Bio Part 1',
+      description: 'Paragraphs(s) before a list of certifications',
+      validation: Rule => Rule.required()
+    },    
+    {
+        name: 'certifications',
+        title: 'Certifications, Memberships & Licenses',
+        type: 'array',
+        of: [
+            {
+            type: 'string'
+            }
+        ]
+    },
+    {
+      name: 'bioB',
+      type: 'string',
+      title: 'Bio Part 2',
+      description: 'Paragraph(s) after a list of certifications'
+    }
+  ],
+  preview: {
+    select: {
+      title: 'name',
+      media: 'image'
+    },
+    prepare ({title = 'No title', media}) {
+      return {
+        title,
+        media
+      }
+    }
+  }
+}
